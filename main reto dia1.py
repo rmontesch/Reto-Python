8be7229ff52645e00ae7c8856e123043f5383093
rmontesch <158780525+rmontesch@users.noreply.github.com>
2024-02-07 22:26:04 -0600

    Reto día 1
    inicié tarde el reto, pero enviaré todos los retos vencidos y me pondré al corriente ASAP

---
+++ main reto dia1.py
@@ -0,0 +1,11 @@
+# Reto dia 1
+
+names = input('Ingresa tu nombre(s) : ')
+last_name = input('ingresa tus apellidos :')
+full_name = names + ' ' + last_name
+telephone_number = int(input('Ingresa tu número telefónico :'))
+email = input('Ingresa tu correo electrónico :')
+
+print ('Hola ' + full_name +', en breve recibiras un correo a '+ email)
+
+
